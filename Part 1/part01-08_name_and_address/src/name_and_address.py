b04271d4cdfeb335bde2b04dbb80c1d9e9f4681c
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-11-12 14:18:58 +0400

    :heavy_check_mark: 15 exercises for part 1 added

---
+++ Part 1/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,8 @@
+# Write your solution here
+given_name = input("Given name: ")
+family_name = input("Family name: ")
+street_address = input("Street Address: ")
+city_postal_code = input("City and postal code: ")
+print(given_name + " " + family_name)
+print(street_address)
+print(city_postal_code)
